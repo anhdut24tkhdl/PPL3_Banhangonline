e765e5d6343e34879e1b56d25b2a2afd29be7d7d
Nguyen <nguyennbkun@gmail.com>
2026-04-14 05:01:59 +0700

    thong ke admin

--- PPL3_Banhangonline/Controllers/AdminController.cs
+++ PPL3_Banhangonline/Controllers/AdminController.cs
@@ -1,22 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PPL3_Banhangonline.Database;
+using PPL3_Banhangonline.Models.Viewmodels;
 
 namespace PPL3_Banhangonline.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private IActionResult? KiemTraAdmin()
+        {
+            var role = HttpContext.Session.GetString("Role");
+            if (role?.ToLower() != "admin")
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return null;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var check = KiemTraAdmin();
+            if (check != null)
+            {
+                return check;
+            }
+
+            var model = new AdminDashboardViewModel
+            {
+                SoTaiKhoan = _context.Account.Count(),
+                SoKhachHang = _context.Customers.Count(),
+                SoSeller = _context.Sellers.Count(),
+                SoDanhMuc = _context.Categories.Count(),
+                SoSanPham = _context.Products.Count()
+            };
+
+            return View(model);
         }
 
         public IActionResult CategoryList()
         {
-            return View();
+            var check = KiemTraAdmin();
+            if (check != null)
+            {
+                return check;
+            }
+
+            return RedirectToAction("Index", "Categories");
         }
 
         public IActionResult UserList()
         {
-            return View();
+            var check = KiemTraAdmin();
+            if (check != null)
+            {
+                return check;
+            }
+
+            return RedirectToAction("Index", "UserManager");
         }
     }
 }
---
+++ PPL3_Banhangonline/Models/Viewmodels/AdminDashboardViewModel.cs
@@ -0,0 +1,11 @@
+namespace PPL3_Banhangonline.Models.Viewmodels
+{
+    public class AdminDashboardViewModel
+    {
+        public int SoTaiKhoan { get; set; }
+        public int SoKhachHang { get; set; }
+        public int SoSeller { get; set; }
+        public int SoDanhMuc { get; set; }
+        public int SoSanPham { get; set; }
+    }
+}
